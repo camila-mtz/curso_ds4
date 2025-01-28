317e593d171d40248cce31e95ee299222912402a
camila-mtz <camilamtzvzla@outlook.com>
2025-01-28 10:55:45 -0700

    Agregué grafica_linea() a funciones.py

--- curso_ds4/linea/app.py
+++ curso_ds4/linea/app.py
@@ -1,14 +1,32 @@
 # Calculo de coordenadas de lineas
 import funciones
+import argparse
 
-def main():
-    m = 2
-    b = 3
-    X = [x for x in range(1,11)]
-    Y = [funciones.calcular_y(x, m, b) for x in X]
-    print(f'Enteros:')
-    coordenadas_enteros = list(zip(X,Y))
-    print(coordenadas_enteros)
+def main(m:float, b:float):
+    '''
+    Función principal que calcula las coordenadas de una línea recta
+    Recibimos: m y b
+    Regresa: nada
+    '''
+    #m = 2
+    #b = 3
+    #X = [x for x in range(1,11)]
+    #Y = [funciones.calcular_y(x, m, b) for x in X]
+    #print(f'Enteros:')
+    #coordenadas_enteros = list(zip(X,Y))
+    #print(coordenadas_enteros)
+    X = [x/10.0 for x in range(10,110,5)]
+    Y = [funciones.calcular_y(x,m,b) for x in X]
+    coordenadas_flotantes = list(zip(X,Y))
+    print("Flotantes")
+    print(coordenadas_flotantes)
+    funciones.grafica_linea(X,Y,m,b)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', type = float,
+    help = 'Pendiente de la línea', default = 2.0)
+    parser.add_argument('-b', type = float,
+    help = 'Ordenada al origen', default = 3.0)
+    args = parser.parse_args()
+    main(args.m, args.b)
--- curso_ds4/linea/funciones.py
+++ curso_ds4/linea/funciones.py
@@ -1,22 +1,38 @@
 # Archivo con todas las funciones necesarias para la aplicacion linea
-def calcular_y(x, m, b):
+import matplotlib.pyplot as plt
+
+def calcular_y(x:float, m:float, b:float)->float:
     '''
     Calcula el valor de y en una línea recta
     x: valor de x
     m: pendiente
     b: intersección en y
-    regresa el valor de y
+    Regresa: el valor de y
     '''
     return (m * x) + b
+
+def grafica_linea(X:list, Y:list, m:float, b:float):
+    '''
+    Gráfica una linea recta
+    X lista de valores de x
+    Y: lista de valores de y
+    Regresa: nada
+    '''
+    plt.plot(X,Y)
+    plt.title(f'Línea con pendiente {m} y ordenada al origen {b}')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
 
 def test_linea():
     '''
     Comprobamos calcular_y
     '''
-    return calcular_y(0,2,3)
+    y = calcular_y(0.0, 2.0, 3.0)
+    return y
 
 if __name__ == '__main__':
-    if test_linea() == 3:
+    if test_linea() == 3.0:
         print('Todo bien')
     else:
         print('Algo salió mal')

47b3814f5698f8db6f0adfdbbee3e59eea646b55
camila-mtz <camilamtzvzla@outlook.com>
2025-02-19 10:47:13 -0700

    Se agrego la clase Sport

---
+++ curso_ds4/games/Sport.py
@@ -0,0 +1,34 @@
+class Sport:
+    '''
+    Clase para representar un deporte
+    '''
+    def __init__(self,name:str,players:int,league:str):
+        """ Constructor de Sport """
+        self.name = name
+        if isinstance(players, int):
+            self.players = players
+        else:
+            self.players = int(players)
+        self.league = league
+
+    def __str__(self)->str:
+        """ Representacion en string de Sport """
+        return f"Sport: {self.name}, {self.players}, {self.league}"
+    
+    def __repr__(self)->str:
+        """ Representacion en string de Sport """
+        return f"Sport(name='{self.name}',players={self.players}, league='{self.league}')"
+    
+    def to_json(self)->dict:
+        """ Convertir Sport a JSON """
+        return {"name":self.name, "players":self.players, "league":self.league}
+    
+if __name__ == "__main__":
+        nfl = Sport("Football", 11, "NFL")
+        print(nfl)
+        print(repr(nfl))
+        print(nfl.to_json())
+        lmp = Sport("Baseball", "9", "LMP")
+        print(lmp)
+        print(repr(lmp))
+        print(lmp.to_json())
